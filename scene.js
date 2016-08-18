880e0b918b484d6d3d4ab6a64ea7152cf24e1554
Sophie Wu <wsy51888@gmail.com>
2016-08-18 16:20:27 -0400

    Update icons and push to twitter

--- scene.js
+++ scene.js
@@ -9,9 +9,26 @@ http://www.crystalcodepalace.com/traceryTut.html
 */
 
 {
-	"origin": [],
-	"starRow":["#star#"],
-	"moonRow":[],
+	"origin": [
+		"#starRow##starRow##starRow#",
+		"#starRow##moonRow##starRow#",
+		"#starRow##starRow##moonRow#"
+	],
+	"starRow":["#star##star##star##star##star##star##star##star##star##star##star##star#\n"],
+	"moonRow":[
+		"#moon##star##star##star##star##star##star##star##star##star##star##star#\n",
+		"#star##moon##star##star##star##star##star##star##star##star##star##star#\n",
+		"#star##star##moon##star##star##star##star##star##star##star##star##star#\n",
+		"#star##star##star##moon##star##star##star##star##star##star##star##star#\n",
+		"#star##star##star##star##moon##star##star##star##star##star##star##star#\n",
+		"#star##star##star##star##star##moon##star##star##star##star##star##star#\n",
+		"#star##star##star##star##star##star##moon##star##star##star##star##star#\n",
+		"#star##star##star##star##star##star##star##moon##star##star##star##star#\n",
+		"#star##star##star##star##star##star##star##star##moon##star##star##star#\n",
+		"#star##star##star##star##star##star##star##star##star##moon##star##star#\n",
+		"#star##star##star##star##star##star##star##star##star##star##moon##star#\n",
+		"#star##star##star##star##star##star##star##star##star##star##star##moon#\n"
+	],
 	"star" : [
 		"✢",
 		"✥",
